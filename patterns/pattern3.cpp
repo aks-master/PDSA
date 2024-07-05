ad3518ab7c39a26c2563fc57b9906156c134180a
Abhishek Kumar Singh <abhishekse@outlook.com>
2024-07-05 17:38:17 +0530

    pttern3 added

---
+++ patterns/pattern3.cpp
@@ -0,0 +1,29 @@
+/*
+for n=5
+     *
+    **
+   ***
+  ****
+ *****
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int spaces = n; spaces > i; spaces--)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
